7aa4802efb949f96fc78160c080b98486d46ecda
Abhishek Bahuguna <abhishek@pearlorganisation.com>
2024-05-30 11:26:26 +0530

    setup shad cn

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,9 @@
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const Home = () => {
     return (
-        <div className='min-h-screen grid place-items-center'>Home</div>
+        <div className='min-h-screen grid place-items-center'><Button>Button</Button></div>
     )
 }
 
